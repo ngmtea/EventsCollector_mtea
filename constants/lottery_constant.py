143fc0db46537694b9be75ec4dd2e73b44141340
phamvietbang <superpower99x99@gmail.com>
2022-08-23 17:04:16 +0700

    add lotery

--- constants/lottery_constant.py
+++ constants/lottery_constant.py
@@ -1,4 +1,5 @@
 class LotteryConstant:
     chain_id = {
-        "0x359f798049562a552142cbeff4e30eba844aae5f": "0x61"
+        "0x359f798049562a552142cbeff4e30eba844aae5f": "0x61",
+        "0x3d81662b14ff6c48ad13809f4d92183bb23d7823": "0x61"
     }
--- services/update_missed_event.py
+++ services/update_missed_event.py
@@ -1,23 +1,27 @@
 from configs.config import *
+import json
 
 
 def run(params):
     str = f"""bash update_missed_event.sh """
-    str += params["start_block"]+" "
-    str += params["end_block"]+" "
-    str += params["address"]+" "
-    str += params["provider"]+" "
-    str += params["abi"]+" "
-    str += params["mongo_db"]+" "
-    str += params["arrango_db"]+" "
-    str += params['last_synced_block']+" "
-    str += params["enricher_id"]+" "
-    str += params["timestamp"]+" "
-    str += params["chain_id"]+" "
-    str += params["oracle_address"]+" "
+    str += params["start_block"] + " "
+    str += params["end_block"] + " "
+    str += params["address"] + " "
+    str += params["provider"] + " "
+    str += params["abi"] + " "
+    str += params["mongo_db"] + " "
+    str += params["arrango_db"] + " "
+    str += params['last_synced_block'] + " "
+    str += params["enricher_id"] + " "
+    str += params["timestamp"] + " "
+    str += params["chain_id"] + " "
+    str += params["oracle_address"] + " "
     str += params["db_prefix"]
     # print(str)
     # str += "15041790 15051890 0x75DE5f7c91a89C16714017c7443eca20C7a8c295 https://nd-384-319-366.p2pify.com/ v1 "
     # str += "mongodb://localhost:27017/ arangodb@root:20011999@http://localhost:8529 last_synced_block.txt enricher-default-id 1644219999 0x38 0x7cd53b71bf56cc6c9c9b43719fe98e7c360c35df"
 
     os.system(str)
+
+
+
