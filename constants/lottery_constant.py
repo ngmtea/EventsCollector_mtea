4e5da857d7376ab9c17917e0f4a4f18a7d2fd4a6
phamvietbang <superpower99x99@gmail.com>
2022-08-30 16:29:03 +0700

    push new lottery

--- constants/lottery_constant.py
+++ constants/lottery_constant.py
@@ -1,9 +1,11 @@
 class LotteryConstant:
     chain_id = {
         "0x359f798049562a552142cbeff4e30eba844aae5f": "0x61",
-        "0x3d81662b14ff6c48ad13809f4d92183bb23d7823": "0x61"
+        "0x3d81662b14ff6c48ad13809f4d92183bb23d7823": "0x61",
+        "0xaabb35e80a093178356f3c55bd43c985ed886338":0x61
     }
     decimals = {
         "0x359f798049562a552142cbeff4e30eba844aae5f":18,
+        "0xaabb35e80a093178356f3c55bd43c985ed886338":18,
         "0x3d81662b14ff6c48ad13809f4d92183bb23d7823":6
     }
