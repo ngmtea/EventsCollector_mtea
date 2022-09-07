39b3f19cb406cc6ee6299c675e5b84fbb672934e
phamvietbang <superpower99x99@gmail.com>
2022-09-07 14:56:25 +0700

    add token

--- constants/lottery_constant.py
+++ constants/lottery_constant.py
@@ -3,11 +3,13 @@ class LotteryConstant:
         "0x359f798049562a552142cbeff4e30eba844aae5f": "0x61",
         "0x3d81662b14ff6c48ad13809f4d92183bb23d7823": "0x61",
         "0xaabb35e80a093178356f3c55bd43c985ed886338": "0x61",
+        "0xcb6edc75926700c07956caad6b8ed6617bc34060": "0x61",
         "0x628cd6306b2742b01490fc1cb47e06a3b72352b3": "0x38"
     }
     decimals = {
         "0x359f798049562a552142cbeff4e30eba844aae5f": 18,
         "0xaabb35e80a093178356f3c55bd43c985ed886338": 18,
+        "0xcb6edc75926700c07956caad6b8ed6617bc34060": 18,
         "0x3d81662b14ff6c48ad13809f4d92183bb23d7823": 6,
         "0x628cd6306b2742b01490fc1cb47e06a3b72352b3": 18
     }
